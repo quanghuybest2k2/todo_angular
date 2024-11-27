6b7f6d6884ee213b12670f8b71e2ff591776e685
dominhdang148 <myclone235@gmail.com>
2024-11-28 00:05:29 +0700

    first init

---
+++ src/app/models/status.ts
@@ -0,0 +1,6 @@
+export enum Status {
+  IN_PROGRESS,
+  IMPORTANT,
+  DONE,
+  ARCHIVED,
+}
---
+++ src/app/models/task.ts
@@ -0,0 +1,8 @@
+import { Status } from './status';
+
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  status: Status;
+}
